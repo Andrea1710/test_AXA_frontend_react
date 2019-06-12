cdd25c7f473b4361d9de188f1385ce97eb286dca
Andrea <andrea.belluccia@gmail.com>
2019-06-12 15:24:28 +0200

    Fetched JSON data from Endpoint

--- src/containers/Gnomes.js
+++ src/containers/Gnomes.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 
 class Gnomes extends Component {
+  state = {
+    gnomes: []
+  };
+
+  componentDidMount() {
+    this.getGnomes();
+  }
+
+  getGnomes = () => {
+    fetch(
+      "https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json"
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(resData => {
+        const data = resData.Brastlewark;
+        this.setState({ gnomes: data });
+      });
+  };
+
   render() {
+    console.log(this.state.gnomes);
+
     return (
       <div>
         <p>Gnomes List</p>
