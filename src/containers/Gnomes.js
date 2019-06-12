8ae083273bb86e9459e1cae88a17dfc0324320b6
Andrea <andrea.belluccia@gmail.com>
2019-06-12 15:14:24 +0200

    Initial Commit - Boilerplate

---
+++ src/containers/Gnomes.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Gnomes extends Component {
+  render() {
+    return (
+      <div>
+        <p>Gnomes List</p>
+      </div>
+    );
+  }
+}
+
+export default Gnomes;
